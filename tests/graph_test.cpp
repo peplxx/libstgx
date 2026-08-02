675c64150715e90dcea8af5286a085f9c36e37a7
Melnikov Sergei <91543105+peplxx@users.noreply.github.com>
2026-08-02 18:50:33 +0300

    feat: add vscode (build / lint / test) tasks (#1)

--- tests/graph_test.cpp
+++ tests/graph_test.cpp
@@ -1,5 +1,6 @@
-#include <catch2/catch_test_macros.hpp>
 #include <stgx/graph.hpp>
+
+#include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("Graph default construction", "[graph]") {
   stgx::Graph g;
